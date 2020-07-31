da828569933ee21b2e51c4c3399781ceb9f08329
Bryan Chen <bryanchen74@gmail.com>
2020-07-31 15:15:53 -0400

    Update scrape command

--- src/commands/ScrapeCommand.ts
+++ src/commands/ScrapeCommand.ts
@@ -1,11 +1,8 @@
-import { Client, Command, CommandMessage, Validator } from "its-not-commando";
 import Discord from "discord.js";
-import { Message, MessageInterface, DatabaseFuncs, User } from "../database";
-import { promisify } from "util";
-import { Util } from "../util";
-import mongoose from "mongoose";
+import { Client, Command, CommandMessage, Validator } from "its-not-commando";
 import { DISCORD_SERVER_ID } from "../config";
-const sleep = promisify(setTimeout);
+import { DatabaseFuncs, Message, User } from "../database";
+import { Util } from "../util";
 
 type ChannelRemaining = {
   channel: Discord.TextChannel;
@@ -13,74 +10,92 @@ type ChannelRemaining = {
 };
 
 const LIMIT = 100;
-async function scrape(guild: Discord.Guild, msg: CommandMessage) {
-  const channels: ChannelRemaining[] = guild.channels.cache
-    .array()
-    .filter((c) => c instanceof Discord.TextChannel)
-    .map((c) => ({
-      channel: c as Discord.TextChannel,
-      before: (c as Discord.TextChannel).lastMessageID ?? undefined,
-    }));
+async function scrape(
+  guild: Discord.Guild,
+  msg: CommandMessage,
+  channelIDs?: string[]
+) {
+  let round: number,
+    msgText: string,
+    numMessages: number,
+    totalMessages: number;
+
+  // Get a list of channels
+  let channels: ChannelRemaining[] = [];
+  if (channelIDs) {
+    channels = channelIDs
+      .map((c) => guild.channels.resolve(c))
+      .filter((c) => c instanceof Discord.TextChannel)
+      .map((c) => ({
+        channel: c as Discord.TextChannel,
+        before: undefined,
+      }));
+  } else {
+    channels = guild.channels.cache
+      .array()
+      .filter((c) => c instanceof Discord.TextChannel)
+      .map((c) => ({
+        channel: c as Discord.TextChannel,
+        before: undefined,
+      }));
+  }
+  channels.sort((a, b) => a.channel.position - b.channel.position);
+
+  msgText = `Scraping ${channels.length} channels: ${channels
+    .map((c) => c.channel.name)
+    .join(", ")}`;
+
+  console.log(msgText);
+  await msg.say(msgText);
+
+  // Scrape each channel
+  totalMessages = 0;
   const numChannels = channels.length;
-  console.log(
-    "Scraping",
-    numChannels,
-    "channels:\n",
-    channels.map((c) => c.channel.name).join(", ")
-  );
-  let round = 0;
-  let numMessages = 0;
-  while (channels.length > 0) {
-    round++;
-    const statusText =
-      "Round " + round + " with " + channels.length + " channels";
-    console.log(statusText);
-    msg.say(statusText);
-    const promises: Promise<Discord.Collection<string, Discord.Message>>[] = [];
-    for (const channel of channels) {
-      promises.push(
-        channel.channel.messages.fetch({
-          limit: LIMIT,
-          before: channel.before,
-        })
-      );
-    }
-    const results = await Promise.all(promises);
-    for (const res of results) {
-      for (const msg of res.array()) {
+  for (let i = 0; i < numChannels; i++) {
+    const channel = channels[i];
+    msgText = `${i + 1}/${numChannels} Scraping #${channel.channel.name} (${
+      channel.channel.id
+    })`;
+    console.log(msgText);
+    await msg.say(`**${msgText}**`);
+    round = 0;
+
+    // Scrape messages from channel
+    do {
+      round++;
+      numMessages = 0;
+      const res = await channel.channel.messages.fetch({
+        limit: LIMIT,
+        before: channel.before,
+      });
+      for (const m of res.array()) {
         await DatabaseFuncs.addMessage({
-          messageID: msg.id,
-          channelID: msg.channel.id,
-          userID: msg.author.id,
-          username: msg.author.username ?? "DeletedUser",
-          content: msg.content,
-          contentWords: Util.findWords(msg.content),
+          messageID: m.id,
+          channelID: m.channel.id,
+          userID: m.author.id,
+          username: m.author.username ?? "DeletedUser",
+          content: m.content,
+          contentWords: Util.findWords(m.content),
         });
         numMessages++;
+        totalMessages++;
       }
-    }
-    for (let i = results.length - 1; i >= 0; i--) {
-      if (results[i].size !== LIMIT) {
-        channels.splice(i, 1);
-      } else {
-        channels[i].before = results[i].last()?.id;
+      channel.before = res.last()?.id;
+      msgText = `Round ${round}: ${numMessages} messages`;
+      console.log(msgText);
+      if (round % 10 === 1 || numMessages !== LIMIT) {
+        await msg.say(msgText);
       }
-    }
+    } while (numMessages === LIMIT);
   }
-  console.log(
-    "Successfully scraped",
-    numMessages,
-    "messages from",
-    numChannels,
-    "channels"
-  );
-  msg.say(
+  msgText =
     "Successfully scraped " +
-      numMessages +
-      " messages from " +
-      numChannels +
-      " channels "
-  );
+    totalMessages +
+    " messages from " +
+    numChannels +
+    " channels ";
+  console.log(msgText);
+  await msg.say(msgText);
 }
 
 export default class ScrapeCommand extends Command {
@@ -92,35 +107,39 @@ export default class ScrapeCommand extends Command {
       ownerOnly: true,
       arguments: [
         {
-          name: "confirm",
+          name: "channels-or-confirm",
           optional: true,
-          defaultValue: "no",
-        },
-        {
-          name: "clear-cache",
-          validator: Validator.Boolean,
-          optional: true,
-          defaultValue: "true",
+          multi: true,
         },
       ],
     });
   }
   public async run(msg: CommandMessage, args: string[], client: Client) {
     const guild = client.guilds.resolve(DISCORD_SERVER_ID);
-    if (args[0] !== "yes") {
-      msg.say("You must confirm using `b.scrape yes`");
-      return;
-    }
-    const clearCache = args[1];
-    if (clearCache) {
-      msg.say("Clearing databases...");
-      await Promise.all([Message.collection.drop(), User.collection.drop()]);
-    }
     if (!guild) {
       msg.say("Could not find guild with id " + args[0]);
       return;
     }
+    let channels: string[] | undefined;
+    let yes = false;
+    if (args[0] === "yes") {
+      msg.say("Clearing databases...");
+      await Promise.all([Message.collection.drop(), User.collection.drop()]);
+      yes = true;
+    } else {
+      channels = args[1]?.split(" ");
+    }
+    if (!channels && !yes) {
+      msg.say(
+        "You must use `b.scrape yes` or `b.scrape 123124 123125424 123124`..."
+      );
+      return;
+    }
     msg.say("Starting to scrape " + guild.name + "...");
-    await scrape(guild, msg);
+    try {
+      await scrape(guild, msg, channels);
+    } catch (error) {
+      msg.say(error + "");
+    }
   }
 }
